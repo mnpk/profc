7709701ee9906b9e43741e44218db0c795f73aab
mnpk <mnncat@gmail.com>
2014-07-09 17:45:40 +0900

    added example

---
+++ example.cpp
@@ -0,0 +1,17 @@
+#include "profc.h"
+
+void Foo() {
+  PROFC_NODE("Foo");
+  std::this_thread::sleep_for(std::chrono::milliseconds(1));
+}
+void Bar() {
+  PROFC_NODE("Bar");
+  std::this_thread::sleep_for(std::chrono::microseconds(50));
+}
+
+int main() {
+  Foo();
+  Foo();
+  Bar();
+  return 0;
+}
